3a4496942388616bb8af1660c4c07c847f4c4d97
NAT <nat@magicnat.com>
2017-06-06 01:20:40 +0800

    init.

---
+++ js/app.js
@@ -0,0 +1,94 @@
+var rs_ctrl = (function () {
+
+  var filter, _filter, view, input, serv_id, max_rslt;
+
+  document.getElementById('input').addEventListener('keydown', e => { if(e.keyCode == 13) rs_ctrl.update_view() });
+  document.getElementById('max_res').addEventListener('keydown', e => { if(e.keyCode == 13) rs_ctrl.update_view() });
+
+  var router_servers = [
+    { name: 'Vultr JP (IPv6)',
+      url: 'http://141.193.21.2/routes6.json', 
+      data: [],
+      id: 0,
+      ipv6: true
+    },
+    { name: 'Hurricane Electric HK (IPv6)',
+      url: 'http://123.103.252.145/routes.json',
+      data: [],
+      id: 1,
+      ipv6: true
+    }
+  ];
+
+  var fetch_data = url => new Promise((res, rej) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = function () {
+      if (this.status == 200) {
+        res(JSON.parse(xhr.response).filter(r => r.as_path));
+      } else rej(xhr.statusText);
+    };
+    xhr.onerror = err => rej(err);
+    xhr.send();
+  });
+
+  router_servers.forEach(rs => {
+    fetch_data(rs.url).then(r => {
+      rs.data = r;
+      var sel = document.createElement('option');
+      sel.setAttribute('value', rs.id);
+      sel.innerHTML = rs.name;
+      document.getElementById('server_id').appendChild(sel);
+    });
+  });
+
+  var get_routes = server_id => {
+    try {
+      return router_servers[server_id].data.filter(r => eval(input.value));
+    } catch (e) {
+      console.error('Filter Error: ' + e);
+    }
+  };
+
+  var tr = function(tds) {
+    var resultTR = document.createElement('tr');
+    tds.forEach(td => {
+      var resultTD = document.createElement('td');
+      resultTD.innerHTML = td.v;
+      resultTD.className = td.c;
+      resultTR.appendChild(resultTD);
+    });
+    return resultTR;
+  };
+
+  var update_view = function() {
+    view = document.getElementById('view');
+    input = document.getElementById('input');
+    serv_id = document.getElementById('server_id');
+    max_rslt = document.getElementById('max_res');
+    var resultHTML = document.createElement('div');
+    resultHTML.className = 'disp';
+    var results = get_routes(serv_id.value);
+    var max = Number.parseInt(max_rslt.value);
+    max = Number.isInteger(max) && max > 0 ? max : 10;
+    results = results.slice(-1 * max);
+    results.forEach(rou => {
+      var table = document.createElement('table');
+      var tbody = document.createElement('tbody');
+      tbody.appendChild(new tr([{c: 't_route', v: rou.route}]));
+      tbody.appendChild(new tr([{c: 't_dhead', v: 'Via'}, {c: 't_var', v: rou.via}]));
+      tbody.appendChild(new tr([{c: 't_dhead', v: 'Device'}, {c: 't_var', v: rou.dev}]));
+      tbody.appendChild(new tr([{c: 't_dhead', v: 'AS Path'}, {c: 't_var', v: rou.as_path.toString()}]));
+      tbody.appendChild(new tr([{c: 't_dhead', v: 'Nexthop'}, {c: 't_var', v: rou.next_hop.toString()}]));
+      if (rou.bgp_community) tbody.appendChild(new tr([{c: 't_dhead', v: 'Community'}, {c: 't_var', v: rou.bgp_community.toString()}]));
+      table.appendChild(tbody);
+      resultHTML.appendChild(table);
+    });
+    view.innerHTML = '';
+    view.appendChild(resultHTML);
+  };
+
+  return {
+    update_view
+  };
+})();
